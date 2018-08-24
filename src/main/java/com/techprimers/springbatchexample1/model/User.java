b02e36b8b175b2da1608d4fbee8b5bf27ee13598
Dhananjay <sharmadhananjay42@gmail.com>
2018-08-25 02:42:41 +0530

    1st basic csv to DB save batch, further changes will come for sure

---
+++ src/main/java/com/techprimers/springbatchexample1/model/User.java
@@ -0,0 +1,33 @@
+package com.techprimers.springbatchexample1.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Table(name = "user")
+@Entity
+@Data
+public class User {
+
+	@Id
+	private Integer id;
+	private String name;
+	private String dept;
+	private Integer salary;
+	private Date time;
+
+	@Override
+	public String toString() {
+		final StringBuffer sb = new StringBuffer("User{");
+		sb.append("id=").append(id);
+		sb.append(", name='").append(name).append('\'');
+		sb.append(", dept='").append(dept).append('\'');
+		sb.append(", salary=").append(salary + "\ntime=" + time);
+		sb.append('}');
+		return sb.toString();
+	}
+}
